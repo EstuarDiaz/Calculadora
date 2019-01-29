8e51771ba40db825dd9d75a871c9db4187a5796a
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-01-29 09:27:16 -0600

    Calculadora
    Funcionalidad de la calculadora en el Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
-
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		iPila<String> miStack = new Pila<String>();
-	    miStack.push("Adios");  //1
-		miStack.push("Hola");   //2
-
-	    String cadena = miStack.pop();
-	    System.out.println(cadena);
-		cadena = miStack.pop();
-		System.out.println(cadena);
+		iPila<String> stack = new Pila<String>();
+		iCalculadora calc = new Calculadora();
+		Scanner input = new Scanner(System.in);
+		// Obtener el texto postfix
+		System.out.println("Por favor ingrese el texto");
+        String[] texto = input.nextLine().split(" ");
+        // Guardar el texto en el stack
+        for (int i = texto.length-1; i >= 0; i--) {
+            stack.push(texto[i]);
+         }
+        // Operar el stack con la calculadora
+        while(stack.size() >= 3) {
+        	double a = Double.parseDouble(stack.pop());
+        	double b = Double.parseDouble(stack.pop());
+        	double awnser = 0;
+        	String operacion = stack.pop();
+        	switch(operacion) {
+        		case "+": awnser = calc.sumar(a, b); break;
+        		case "-": awnser = calc.restar(a, b); break;
+        		case "*": awnser = calc.multiplicar(a, b); break;
+        		case "/": awnser = calc.dividir(a, b); break;
+        		default : System.out.println("El texto ingresado no es valido\n");
+			        		return; 
+	        }
+        	stack.push(String.valueOf(awnser));
+        }
+        System.out.println("Respuesta: "+stack.pop());
+        input.close();
 	}
 }
